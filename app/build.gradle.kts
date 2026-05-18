7f39e25223bf705e486b7bb054b809f41a45dca4
lisiupython <774279254@qq.com>
2026-05-19 01:22:18 +0800

    Change build type from debug to release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ lsparanoid {
 }
 
 val apkId = "HyperCeiler"
-val buildTypes = "debug"
+val buildTypes = "release"
 val roots = mapOf(
     "animation" to "libs/animation-${buildTypes}.aar",
     "appcompat" to "libs/appcompat-${buildTypes}.aar",
